455aec3bed30fbf149d9f851085a3878f17675ee
Teresa Acosta <ignacioibntz04@gmail.com>
2025-06-26 19:58:02 -0300

    Primer commit

--- server.py
+++ server.py
@@ -31,5 +31,6 @@ def nosotros():
     return render_template('nosotros.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
